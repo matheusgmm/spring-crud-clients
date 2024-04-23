8319f0645fc2a5a8a974ea5a611a2566300ca35c
Matheus Muccio <matheus.muccio@rodojacto.com.br>
2024-04-23 08:11:49 -0300

    Create client

--- src/main/java/com/muccio/crudclient/controllers/ClientController.java
+++ src/main/java/com/muccio/crudclient/controllers/ClientController.java
@@ -6,10 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/clients")
@@ -28,5 +28,13 @@ public class ClientController {
     public ResponseEntity<Page<ClientDTO>> findAll(Pageable pageable) {
         Page<ClientDTO> dto = service.findAll(pageable);
         return ResponseEntity.ok(dto);
+    }
+
+    @PostMapping
+    public ResponseEntity<ClientDTO> insert(@RequestBody ClientDTO client) {
+        client = service.insert(client);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(client.getId()).toUri();
+        return ResponseEntity.created(uri).body(client);
     }
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,20 +1,23 @@
-insert into tb_client (name, cpf, income, birth_date, children) values ('Silvain Gethings', '622-13-7258', 6182.78, '2023-10-26', 1);
-insert into tb_client (name, cpf, income, birth_date, children) values ('Nedda Dallison', '190-91-3315', 3415.24, '2023-12-12', 3);
-insert into tb_client (name, cpf, income, birth_date, children) values ('Cass Elliston', '706-86-4142', 2772.53, '2024-04-21', 1);
-insert into tb_client (name, cpf, income, birth_date, children) values ('Stillmann I''anson', '350-97-1576', 2534.34, '2024-04-05', 0);
-insert into tb_client (name, cpf, income, birth_date, children) values ('Dillon Taleworth', '656-27-5737', 6873.39, '2024-02-19', 2);
-insert into tb_client (name, cpf, income, birth_date, children) values ('Agnella Shelmerdine', '208-42-4968', 2731.91, '2023-08-06', 2);
-insert into tb_client (name, cpf, income, birth_date, children) values ('Engelbert Breheny', '850-65-0996', 2458.62, '2023-04-28', 4);
-insert into tb_client (name, cpf, income, birth_date, children) values ('Gerry Kneafsey', '513-23-7852', 2616.36, '2023-08-18', 1);
-insert into tb_client (name, cpf, income, birth_date, children) values ('Truman Schild', '780-18-0893', 5281.80, '2023-06-10', 3);
-insert into tb_client (name, cpf, income, birth_date, children) values ('Shandeigh Gaylord', '239-55-4572', 1294.27, '2023-05-30', 2);
-insert into tb_client (name, cpf, income, birth_date, children) values ('Sioux Abramzon', '513-35-5390', 3471.03, '2024-01-21', 0);
-insert into tb_client (name, cpf, income, birth_date, children) values ('Drona Wannan', '236-50-7295', 6642.57, '2023-05-29', 1);
-insert into tb_client (name, cpf, income, birth_date, children) values ('Riobard Gadsden', '585-43-3439', 962.49, '2023-11-27', 2);
-insert into tb_client (name, cpf, income, birth_date, children) values ('Celestina Zanettini', '893-96-4217', 3056.24, '2024-04-09', 5);
-insert into tb_client (name, cpf, income, birth_date, children) values ('Klemens Paradis', '218-85-2505', 3579.43, '2023-11-27', 2);
-insert into tb_client (name, cpf, income, birth_date, children) values ('Anestassia Burless', '690-87-0214', 3590.20, '2024-01-04', 1);
-insert into tb_client (name, cpf, income, birth_date, children) values ('Brice Ticksall', '805-51-7904', 3827.61, '2023-06-01', 3);
-insert into tb_client (name, cpf, income, birth_date, children) values ('Norbert Swainsbury', '763-88-8538', 7471.18, '2023-05-31', 3);
-insert into tb_client (name, cpf, income, birth_date, children) values ('Sky Denisovo', '688-04-1529', 7290.25, '2023-11-26', 1);
-insert into tb_client (name, cpf, income, birth_date, children) values ('Jacob Sherville', '745-82-0242', 1979.20, '2023-09-11', 2);
+
+
+
+insert into tb_client (name, cpf, income, birth_date, children) values ('Silvain Gethings', '622-13-7258', 6182.78, '1984-10-26', 1);
+insert into tb_client (name, cpf, income, birth_date, children) values ('Nedda Dallison', '190-91-3315', 3415.24, '1990-12-12', 3);
+insert into tb_client (name, cpf, income, birth_date, children) values ('Cass Elliston', '706-86-4142', 2772.53, '1978-04-21', 1);
+insert into tb_client (name, cpf, income, birth_date, children) values ('Stillmann I''anson', '350-97-1576', 2534.34, '2001-04-05', 0);
+insert into tb_client (name, cpf, income, birth_date, children) values ('Dillon Taleworth', '656-27-5737', 6873.39, '1990-02-19', 2);
+insert into tb_client (name, cpf, income, birth_date, children) values ('Agnella Shelmerdine', '208-42-4968', 2731.91, '1987-08-06', 2);
+insert into tb_client (name, cpf, income, birth_date, children) values ('Engelbert Breheny', '850-65-0996', 2458.62, '1998-04-28', 4);
+insert into tb_client (name, cpf, income, birth_date, children) values ('Gerry Kneafsey', '513-23-7852', 2616.36, '1964-08-18', 1);
+insert into tb_client (name, cpf, income, birth_date, children) values ('Truman Schild', '780-18-0893', 5281.80, '2000-06-10', 3);
+insert into tb_client (name, cpf, income, birth_date, children) values ('Shandeigh Gaylord', '239-55-4572', 1294.27, '1994-05-30', 2);
+insert into tb_client (name, cpf, income, birth_date, children) values ('Sioux Abramzon', '513-35-5390', 3471.03, '1998-01-21', 0);
+insert into tb_client (name, cpf, income, birth_date, children) values ('Drona Wannan', '236-50-7295', 6642.57, '2001-05-29', 1);
+insert into tb_client (name, cpf, income, birth_date, children) values ('Riobard Gadsden', '585-43-3439', 962.49, '1997-11-27', 2);
+insert into tb_client (name, cpf, income, birth_date, children) values ('Celestina Zanettini', '893-96-4217', 3056.24, '1993-04-09', 5);
+insert into tb_client (name, cpf, income, birth_date, children) values ('Klemens Paradis', '218-85-2505', 3579.43, '1999-11-27', 2);
+insert into tb_client (name, cpf, income, birth_date, children) values ('Anestassia Burless', '690-87-0214', 3590.20, '1994-01-04', 1);
+insert into tb_client (name, cpf, income, birth_date, children) values ('Brice Ticksall', '805-51-7904', 3827.61, '1985-06-01', 3);
+insert into tb_client (name, cpf, income, birth_date, children) values ('Norbert Swainsbury', '763-88-8538', 7471.18, '1978-05-31', 3);
+insert into tb_client (name, cpf, income, birth_date, children) values ('Sky Denisovo', '688-04-1529', 7290.25, '1990-11-26', 1);
+insert into tb_client (name, cpf, income, birth_date, children) values ('Jacob Sherville', '745-82-0242', 1979.20, '2001-09-11', 2);
