d871b4d45316ccd981b69315a57a80fec8c728a8
Matheus Muccio <matheus.muccio@rodojacto.com.br>
2024-04-23 07:53:50 -0300

    Find By ID and seeding database

--- src/main/java/com/muccio/crudclient/controllers/ClientController.java
+++ src/main/java/com/muccio/crudclient/controllers/ClientController.java
@@ -1,9 +1,24 @@
 package com.muccio.crudclient.controllers;
 
+import com.muccio.crudclient.dtos.ClientDTO;
+import com.muccio.crudclient.services.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/clients")
 public class ClientController {
+
+    @Autowired
+    private ClientService service;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
+        ClientDTO dto = service.findById(id);
+        return ResponseEntity.ok(dto);
+    }
 }
--- src/main/java/com/muccio/crudclient/services/ClientService.java
+++ src/main/java/com/muccio/crudclient/services/ClientService.java
@@ -1,8 +1,11 @@
 package com.muccio.crudclient.services;
 
+import com.muccio.crudclient.dtos.ClientDTO;
+import com.muccio.crudclient.entities.Client;
 import com.muccio.crudclient.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ClientService {
@@ -10,5 +13,11 @@ public class ClientService {
     @Autowired
     private ClientRepository repository;
 
-    
+    @Transactional(readOnly = true)
+    public ClientDTO findById(Long id) {
+        Client client = repository.findById(id).orElseThrow();
+        return new ClientDTO(client);
+    }
+
+
 }
